bdfb386230778e20cb0d2fb309b5d0652ad1d40d
Ridho Sandhika <zikirnurofficialoffice@gmail.com>
2026-07-16 02:01:59 +0700

    Delete uas pak adilllll directory

--- uas pak adilllll/reset.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'config.php';
-$hash = password_hash('admin123', PASSWORD_DEFAULT);
-$conn->query("UPDATE users SET password = '$hash' WHERE username = 'admin'");
-echo "Password reset to: admin123";
-?>
